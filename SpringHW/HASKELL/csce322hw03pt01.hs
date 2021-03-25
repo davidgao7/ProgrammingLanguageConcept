e0bee7eb333ba591994b850ba85c00d6589c5a10
David Gao <582435572@qq.com>
2021-03-24 23:20:33 -0500

    basic set up done, need to implement clockwise function and test it

--- SpringHW/HASKELL/csce322hw03pt01.hs
+++ SpringHW/HASKELL/csce322hw03pt01.hs
@@ -12,5 +12,22 @@ main = do
  printMaze (onePlayerOneRotation maze (head moves))
 
 -- YOUR CODE SHOULD COME AFTER THIS POINT
-onePlayerOneRotation :: [[Char]] -> [Char] -> [[Char]]
-onePlayerOneRotation maze move = maze
+onePlayerOneRotation :: [[Char]] -> Char -> [[Char]]
+
+onePlayerOneRotation [] _ = []
+onePlayerOneRotation maze move
+                    | move == "c"  = clockwise maze
+                    | move == "cc" = cclockwise maze
+                    | move == "180"= flipwise maze
+                    | otherwise    = maze
+
+                    where{
+                      clockwise(x,y)
+                        | maze[x][y] == 'x' = maze[y][length(maze rows) - 1 - x] = 'x'
+                        | maze[x][y] == '-' = maze[y][length(maze rows) - 1 - x] = '-'
+                        | maze[x][y] == 'g' = maze[y][length(maze rows) - 1 - x] = 'g'
+                        | otherwise playerClockwise maze maze[x][y]
+                    }
+
+playerClockwise :: [[Char]] -> Char -> [[Char]] --TODO: 这个方程写完给教授看看 ！！！
+playerClockwise []
