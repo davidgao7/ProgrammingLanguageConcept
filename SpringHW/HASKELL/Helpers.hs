f7a85b95e2a4c8c446176e55fdc77bd7bd273df8
David Gao <582435572@qq.com>
2021-04-06 00:55:39 -0500

    function to move player to the right(1d maze)
    if you want to do two things, make functions for each things

--- SpringHW/HASKELL/Helpers.hs
+++ SpringHW/HASKELL/Helpers.hs
@@ -6,6 +6,10 @@ printMaze ,
 printMove,
 mymap,
 findItem,
+replace1DListAtIndex,
+moveRight,
+clear1dPlayerPerviousPosition,
+getPlayerMoveRightResult
 ) where
 
 import Prelude
@@ -50,3 +54,19 @@ findHelper e (h:t) i
           | (e == h) = i:(findHelper e t (i + 1)) -- find the item at head, put current index at head, and find rest list
           | otherwise= (findHelper e t (i + 1)) -- didn't find , increament index keep finding
 
+clear1dPlayerPerviousPosition :: [Char] -> Int -> [Char]
+clear1dPlayerPerviousPosition maze i = replace1DListAtIndex maze i '-'
+moveRight :: [Char] -> Int -> Char ->[Char]
+moveRight [] _ _ = []
+moveRight [e] _ _ = [e]
+moveRight list n player
+ | (list !! n /= 'x') = moveRight list (n+1) player
+ | otherwise = replace1DListAtIndex list (n-1) player
+getPlayerMoveRightResult :: [Char] -> Int -> Char-> [Char]
+getPlayerMoveRightResult line n player = clear1dPlayerPerviousPosition (moveRight line n player) n
+replace1DListAtIndex :: [Char] -> Int -> Char -> [Char] -- WORK
+replace1DListAtIndex "" _ _ = ""
+replace1DListAtIndex [e1] 0 e2 = [e2]
+replace1DListAtIndex list n e2
+  | list !! n == e2 = list
+  | otherwise = take n list ++ [e2] ++ drop (n + 1) list
