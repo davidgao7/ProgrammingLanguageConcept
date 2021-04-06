db4e506493e7949b2a682903e285c3aab5ec8e0c
David Gao <582435572@qq.com>
2021-04-06 00:33:13 -0500

    match String input

--- SpringHW/HASKELL/csce322hw03pt01.hs
+++ SpringHW/HASKELL/csce322hw03pt01.hs
@@ -8,26 +8,20 @@ main = do
  args <- getArgs
  filename <- readFile (head args)
  (maze,moves) <- readGravityMazeFile filename
+ print "original"
+ printMaze maze
+ print "moves"
+ print (head moves)
+ print "onePlayerOneRotation: "
  print "Result"
  printMaze (onePlayerOneRotation maze (head moves))
 
 -- YOUR CODE SHOULD COME AFTER THIS POINT
-onePlayerOneRotation :: [[Char]] -> Char -> [[Char]]
+onePlayerOneRotation :: [[Char]] -> [Char] -> [[Char]]
 
 onePlayerOneRotation [] _ = []
 onePlayerOneRotation maze move
-                    | move == "c"  = clockwise maze
-                    | move == "cc" = cclockwise maze
-                    | move == "180"= flipwise maze
+                    | move == "c"  = clockwise maze 0 0
+                    -- | move == "cc" = cclockwiseAll maze
+                    -- | move == "180"= flipAll maze
                     | otherwise    = maze
-
-                    where{
-                      clockwise(x,y)
-                        | maze[x][y] == 'x' = maze[y][length(maze rows) - 1 - x] = 'x'
-                        | maze[x][y] == '-' = maze[y][length(maze rows) - 1 - x] = '-'
-                        | maze[x][y] == 'g' = maze[y][length(maze rows) - 1 - x] = 'g'
-                        | otherwise playerClockwise maze maze[x][y]
-                    }
-
-playerClockwise :: [[Char]] -> Char -> [[Char]] --TODO: 这个方程写完给教授看看 ！！！
-playerClockwise []
