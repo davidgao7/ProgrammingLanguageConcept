2bf3cc08d4c3ad4414d9c9710cd5d2faa931a9c6
David Gao <582435572@qq.com>
2021-04-19 22:03:36 -0500

    check odd rotations work!

--- SpringHW/Prolog/helpers.pl
+++ SpringHW/Prolog/helpers.pl
@@ -1,6 +1,11 @@
 :- module( helpers,
 	 [ readGravityMazeFile/3
 	 , printMazeGame/1
+	 check2DPlayers/2,
+	 check1DPlayers/2,
+	 check2dOddPlayers/1,
+	 check1DRotation/2,
+	 checkOddRotation/1
 	 ]
     ).
 
@@ -27,6 +32,7 @@ printMazeGame([]).
 printMazeGame([Row|Rows]):-
     writeln(Row),
     printMazeGame(Rows).
+
 check1DPlayers([],0).% WORK
 check1DPlayers([E|Rest],Player):-
 	number(E), % WORK
@@ -35,11 +41,22 @@ check1DPlayers([E|Rest],Player):-
 check1DPlayers([E|Rest],Player):-
 	\+ number(E), % WORK
 	check1DPlayers(Rest,Player).% WORK
+
 check2DPlayers([],0). % WORK
 check2DPlayers([Row|Rest],Players):-
 	check1DPlayers(Row,D1Player), % WORK
 	check2DPlayers(Rest,SubPlayers), % WORK
 	Players is SubPlayers + D1Player. % WORK
+
 check2dOddPlayers(Maze):- % WORK
 	check2DPlayers(Maze,Players), % WORK
 	1 is Players mod 2. % WORK
+
+check1DRotation([],0). % WORK
+check1DRotation([_|Rest],Result):- % WORK
+	check1DRotation(Rest, Restresult), % WORK
+	Result is Restresult + 1. % WORK
+
+checkOddRotation(Rotations):- % WORK
+	check1DRotation(Rotations,Result), % WORK
+	1 is Result mod 2. % WORK
