fc7354dcf291ccc89831003eccec0cdc7004de26
David Gao <582435572@qq.com>
2021-05-05 11:21:09 -0500

    cuts:with(out) and outputs

--- prolog/cuts.pl
+++ prolog/cuts.pl
@@ -8,12 +8,74 @@ vowel(u).
     vowel(A),
     vowel(B),
     writeln((A,B)).
+    % ?- myStatement.
+    % a,a
+    % true ;
+    % a,e
+    % true ;
+    % a,i
+    % true ;
+    % a,o
+    % true ;
+    % a,u
+    % true ;
+    % e,a
+    % true ;
+    % e,e
+    % true ;
+    % e,i
+    % true ;
+    % e,o
+    % true ;
+    % e,u
+    % true ;
+    % i,a
+    % true ;
+    % i,e
+    % true ;
+    % i,i
+    % true ;
+    % i,o
+    % true ;
+    % i,u
+    % true ;
+    % o,a
+    % true ;
+    % o,e
+    % true ;
+    % o,i
+    % true ;
+    % o,o
+    % true ;
+    % o,u
+    % true ;
+    % u,a
+    % true ;
+    % u,e
+    % true ;
+    % u,i
+    % true ;
+    % u,o
+    % true ;
+    % u,u
+    % true.
 
     myStatementWithCut:-
-    vowel(A), % before cut, doesn't not have alternate solution
+    vowel(A), % before cut,doesn't not have alternate solution(A doesn't change)
     !,% A won't go to find other solution next line(won't print)
     vowel(B),
     writeln((A,B)).
+    % ?- myStatementWithCut.
+    % a,a
+    % true ;
+    % a,e
+    % true ;
+    % a,i
+    % true ;
+    % a,o
+    % true ;
+    % a,u
+    % true.
 % lowercase : statement
 % Uppercase: variable
 % :- implication
